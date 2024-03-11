d2499ba7559d1fd592cc97272f190f80ab102282
Art <toritosalgado90@gmail.com>
2024-03-10 17:38:38 -0700

    small changes to pooled objects

--- MAIA/Assets/Scripts/ObjectPool/ObjectPool.cs
+++ MAIA/Assets/Scripts/ObjectPool/ObjectPool.cs
@@ -1,39 +1,48 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Object Pool", menuName = "Scriptable Objects/Object Pool", order = 0)]
 public class ObjectPool: ScriptableObject
 {
-    private Stack<GameObject> _pool = new Stack<GameObject>(); 
+    private Stack<PooledObject> _pool = new Stack<PooledObject>(); 
+    private HashSet<PooledObject> _activeObjects = new HashSet<PooledObject>();
     public int pooledCount => this._pool.Count;
+    public int activeCount => this._activeObjects.Count;
+    public int totalCount => this._pool.Count + this._activeObjects.Count;
+
+
     [SerializeField] private GameObject _prefab; 
-    
+
     /// <summary>
-    /// Destroys all currently pooled objects. DOES NOT destroy objects outside the pool.
+    /// Retrieves a pooled object, or creates one if the pool is empty
     /// </summary>
-    public void DestroyPooled() {
-        while (this.pooledCount != 0) {
-            GameObject gameObject = this._pool.Pop();
-            GameObject.Destroy(gameObject);
-        }
-    }
-
+    /// <returns></returns>
     public GameObject Get() {
+        PooledObject pooledObject;
         if (this.pooledCount == 0)
-            return this.Create();
-        return this._pool.Pop();
+            pooledObject = this.Create();
+        else 
+            pooledObject = this._pool.Pop();
+
+        this._activeObjects.Add(pooledObject);
+        pooledObject.gameObject.SetActive(true);
+        return pooledObject.gameObject;
     }
 
-    private GameObject Create() {
+    protected PooledObject Create() {
         GameObject gameObject = GameObject.Instantiate(_prefab);
         GameObject.DontDestroyOnLoad(gameObject);
 
-        // This ensures the integrety of the pool.
-        // The object is passed a reference to the stack (aka pool) upon initialization, this Create() method.
-        // Using its pool reference, it releases itself into its pool reference, which *is* be the pool that set it.
-        gameObject.AddComponent<PooledObject>().Init(this._pool);
+        PooledObject pooledObject = gameObject.AddComponent<PooledObject>();
 
-        return gameObject;
+        // This ensures the integrety of the pool.
+        // The object is passed a reference to the stack (aka pool) in this Create() method.
+        // Using its pool reference, it releases itself into its pool reference, which *is* be the pool that created it.
+        // It also ensures you can't pollute the pool with random objects.        
+        pooledObject.Init(this._pool, this._activeObjects);
+
+        return pooledObject;
     }
     
     // Releasing is handled by the PooledObject component
--- MAIA/Assets/Scripts/ObjectPool/PooledObject.cs
+++ MAIA/Assets/Scripts/ObjectPool/PooledObject.cs
@@ -3,18 +3,22 @@ using UnityEngine;
 
 public class PooledObject : MonoBehaviour
 {
-    private Stack<GameObject> _pool;
+    private Stack<PooledObject> _pool;
+    private HashSet<PooledObject> _active;
 
-    public void Init(Stack<GameObject> pool) {
+    public void Init(Stack<PooledObject> pool, HashSet<PooledObject> activeObjects) {
         if (this._pool == null) {
             this._pool = pool;
-            return;
-        }
-        throw new System.Exception("Can't set pool multiple times on a pooled object!!");
+        } else throw new System.Exception("Can't set the pool multiple times on a pooled object!!");
+        
+        if (this._active == null) {
+            this._active = activeObjects;
+        } else throw new System.Exception("Can't set the active set multiple times on a pooled object!!");
     }
 
     public void Release() {
-        this._pool.Push(this.gameObject);
+        this._pool.Push(this);
+        _active.Remove(this);
         this.gameObject.SetActive(false);
     }
 }
